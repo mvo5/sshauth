61b2f60918a7e5423aa7af1de434d635d2d8b4c4
Joshua M. Clulow <jmc@oxide.computer>
2026-08-31 22:18:27 +1000

    rustfmt

--- src/unverified.rs
+++ src/unverified.rs
@@ -186,7 +186,7 @@ impl UnverifiedTokenVerification {
                 ssh_key::public::KeyData::SkEd25519(pk) => {
                     pk.verify(&blob, &sig).is_ok()
                 }
-ssh_key::public::KeyData::SkEcdsaSha2NistP256(pk) => {
+                ssh_key::public::KeyData::SkEcdsaSha2NistP256(pk) => {
                     pk.verify(&blob, &sig).is_ok()
                 }
                 _ => bail!("unsupported key type: {}", key.algorithm()),
